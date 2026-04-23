62647d848637ddf00f9f2e9e083ca43a42bf077f
Ze'ev Vladimir <zevvladimir3002@gmail.com>
2026-04-23 15:12:50 -0500

    Quartz sync: Apr 23, 2026, 3:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "zeevvladimir.com",
     ignorePatterns: ["private", "templates", ".obsidian", "Files/Papers/**", "Templates/", "*.sync-conflict-*"],
     defaultDateType: "modified",
     theme: {
